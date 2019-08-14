5cd5f92daa07da0a662b62e3d0bb4905add9a6f6
Shujaat Azim <shujaat.azim@gmail.com>
2019-08-14 11:54:40 -0400

    Done.

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+def smallest_multiple(num)
+    arr = (1..num).to_a
+    flag = false
+    counter = num
+  
+    while flag == false
+      counter += num 
+      arr.each do |n|
+        if (counter % n != 0)
+          flag = false
+          break
+        else
+          flag = true
+        end
+      end
+    end
+    counter
+  end
